112d4848e595f8881cf6389c8a34e25bbacdafb3
Kae Bartlett <kae@voxelfox.co.uk>
2026-01-07 02:53:58 +0000

    Add highlight user function

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A small extension with tools to enhance your Commishes experience.
 
 ## Features
 
+* User highlighting
+    * Someone you particularly like? Want to keep track of their auctions from the main auction page? When set up, their listings will be highlighted with a colored border, making them easy to spot.
 * User filtering
     * Have some users that you aren't a fan of? Someone who only draws women? Someone whose art style you just don't gel with? Just add their name to the list of users, and you can either dim their listings or hide them entirely from the masonary.
 * Simple QOL features
--- options.js
+++ options.js
@@ -1,12 +1,15 @@
 async function saveOptions(e) {
     e.preventDefault();
     await browser.storage.sync.set({
+        highlightedUsers: document.querySelector("#highlighted-users").value.trim().split("\n").map(s => s.trim()).filter(s => s.length > 0),
         filteredUsers: document.querySelector("#filtered-users").value.trim().split("\n").map(s => s.trim()).filter(s => s.length > 0),
         filterMode: document.querySelector("#filter-mode").value
     });
 }
 
 async function restoreOptions() {
+    let highlightedUsers = await browser.storage.sync.get("highlightedUsers");
+    document.querySelector("#highlighted-users").value = (highlightedUsers.highlightedUsers || []).join("\n");
     let filteredUsers = await browser.storage.sync.get("filteredUsers");
     document.querySelector("#filtered-users").value = (filteredUsers.filteredUsers || []).join("\n");
     let filterMode = await browser.storage.sync.get("filterMode");
